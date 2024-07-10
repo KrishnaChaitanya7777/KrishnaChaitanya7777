a205a20e60c6966a03f2a34dded7932c80c6e016
KrishnaChaitanya7777 <kc000022@gmail.com>
2024-07-10 17:08:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,34 +7,54 @@ const app = express();
 const port = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/registrationDB', {
+mongoose.connect('mongodb://localhost:27017/blogDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
 });
 
-const User = mongoose.model('User', userSchema);
+const Post = mongoose.model('Post', postSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/register', (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    });
+// Serve HTML files
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-    newUser.save((err) => {
+app.get('/newpost', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'newpost.html'));
+});
+
+// API endpoint to get all posts
+app.get('/posts', (req, res) => {
+    Post.find({}, (err, posts) => {
         if (err) {
             res.send(err);
         } else {
-            res.send('Successfully registered!');
+            res.json(posts);
+        }
+    });
+});
+
+// Handle new post submission
+app.post('/newpost', (req, res) => {
+    const newPost = new Post({
+        title: req.body.title,
+        content: req.body.content,
+    });
+
+    newPost.save((err) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect('/');
         }
     });
 });
